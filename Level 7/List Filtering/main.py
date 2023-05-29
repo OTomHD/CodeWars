ea734e0133a1461bd8930ddc580603ef492c9059
OTomHD <tomquick@live.com>
2023-05-29 23:33:07 +0100

    List Filtering

---
+++ Level 7/List Filtering/main.py
@@ -0,0 +1,11 @@
+"""
+In this kata you will create a function that takes a list of non-negative integers 
+and strings and returns a new list with the strings filtered out.
+"""
+
+def filter_list(l):
+    newArray = []
+    for element in l:
+        if(type(element) is int):
+            newArray.append(element)
+    return newArray
